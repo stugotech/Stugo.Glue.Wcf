b70b466d30499f9a696cf7630f5880d1e5a862af
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2016-07-15 09:34:17 +0100

    include logging

--- Stugo.Glue.Wcf/WcfInstanceProvider.cs
+++ Stugo.Glue.Wcf/WcfInstanceProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stugo.Logging;
+using System;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Dispatcher;
@@ -7,6 +8,7 @@ namespace Stugo.Glue.Wcf
 {
     class WcfInstanceProvider : IInstanceProvider
     {
+        private static readonly ILog logger = Logger.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private readonly IContainer container;
 
 
@@ -25,12 +27,14 @@ namespace Stugo.Glue.Wcf
         public object GetInstance(InstanceContext instanceContext)
         {
             var serviceType = instanceContext.Host.Description.ServiceType;
+            logger.Trace($"Constructing service instance for {serviceType.FullName}");
             return this.container.Resolve(serviceType);
         }
 
 
         public void ReleaseInstance(InstanceContext instanceContext, object instance)
         {
+            logger.Trace($"Releasing service instance of type {instance.GetType().FullName}");
             var disposable = instance as IDisposable;
 
             if (disposable != null)
