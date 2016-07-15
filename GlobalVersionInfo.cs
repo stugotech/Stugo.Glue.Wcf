08638b010c061ae9ac7cc790be3bb5eec17a6514
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2016-07-15 10:13:48 +0100

    bump to v2.1

--- GlobalVersionInfo.cs
+++ GlobalVersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyInformationalVersion("2.0+$REVID_N(12)$.$UTCDATETIME$.$DIRTY$")]
+[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyInformationalVersion("2.1+$REVID_N(12)$.$UTCDATETIME$.$DIRTY$")]
